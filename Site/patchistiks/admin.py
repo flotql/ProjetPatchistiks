15caa5e0660e1f85715131ca1baac92028cd8b3c
Florent T <94051212+flotql@users.noreply.github.com>
2022-04-02 18:13:21 +0200

    update models

--- Site/patchistiks/admin.py
+++ Site/patchistiks/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models import *
 # Register your models here.
+
+
+admin.site.register(ItemType)
+admin.site.register(ItemClass)
+admin.site.register(Make)
+admin.site.register(Items)
+admin.site.register(RoleChamp)
+admin.site.register(Spell)
+admin.site.register(Champions)
+admin.site.register(Effect)
+admin.site.register(Patch)
+admin.site.register(Stats)
+admin.site.register(Affect)
--- Site/patchistiks/models.py
+++ Site/patchistiks/models.py
@@ -1,8 +1,90 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.utils import timezone
 # Create your models here.
 
 
+class ItemType (models.Model):
+    itemTypeName = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.itemTypeName
+
+class ItemClass(models.Model):
+    itemClassRarity = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.itemClassRarity
+
+class Items(models.Model):
+    itemName = models.CharField(max_length=50)
+    chose = models.ManyToManyField(User)
+    got = models.ManyToManyField(ItemType)
+
+
+    def __str__(self):
+        return self.itemName
+
+class RoleChamp(models.Model):
+    roleType = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.roleType
+
 class Spell(models.Model):
-    spellName = models.CharField(max_length=50)
-    spellEffect = models.CharField(max_length=500)
+    spellName = models.CharField(max_length=30)
+    spellBind = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.spellName
+
+class Champions(models.Model):
+    championName = models.CharField(max_length=30)
+    championTitle = models.CharField(max_length=50)
+    are = models.ForeignKey(RoleChamp, on_delete=models.CASCADE)
+    use = models.ForeignKey(Spell, on_delete=models.CASCADE)
+    pick = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.championName
+
+class Effect (models.Model):
+    effectText = models.CharField(max_length=300, blank=None)
+    effectMake = models.ManyToManyField(Items, through='Make')
+    get = models.ManyToManyField(Spell)
+
+    def __str__(self):
+        return self.effectText
+
+class Patch(models.Model):
+    patchNumber = models.CharField(max_length=20)
+    patchDate = models.DateTimeField('Date de publication')
+
+    def __str__(self):
+        return self.patchNumber, self.patchDate
+
+class Stats(models.Model):
+    statName = models.CharField(max_length=50)
+    statValue = models.IntegerField(default=0)
+    will = models.ManyToManyField(Items)
+    modify = models.ManyToManyField(Patch, through='Affect')
+    have = models.ManyToManyField(Champions)
+    put = models.ManyToManyField(Effect)
+
+    def __str__(self):
+        return self.statName, self.statValue
+
+class Affect(models.Model):
+    affectStat = models.DecimalField(max_digits=5, decimal_places=3)
+    stat = models.ForeignKey(Stats, on_delete=models.CASCADE)
+    patch = models.ForeignKey(Patch, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.affectStat
+
+class Make(models.Model):
+    makeText = models.CharField(max_length=500)
+    makeEffect = models.ForeignKey(Effect, on_delete=models.CASCADE)
+    makeItems = models.ForeignKey(Items, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.makeText
